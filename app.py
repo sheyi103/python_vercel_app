252e994901a3d5bfe088bb28f777ade5bae52db3
seyi <sheyi103@outlook.com>
2024-02-06 02:12:37 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/', methods=['GET']) 
+def home():
+     return "Homepage" @app.route('/contact', methods=['GET']) 
+def contact():
+     return "Contact page"
+if __name__ == "__main__":
+     
+    app.run()
+
